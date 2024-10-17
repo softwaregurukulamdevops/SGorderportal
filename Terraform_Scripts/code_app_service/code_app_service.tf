a7d6b54eb8870252254370e4bbfc0f7348f6d786
softwaregurukulamdevops <pramodkotagiri@softwaregurukulam.com>
2024-10-18 00:53:03 +0530

    Update code_app_service.tf

--- Terraform_Scripts/code_app_service/code_app_service.tf
+++ Terraform_Scripts/code_app_service/code_app_service.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 # Define the Resource Group
 resource "azurerm_resource_group" "example" {
   name     = "rg-orderportal-resources"
-  location = "East US"  # Replace with your desired location
+  location = "Canada East"  # Replace with your desired location
 }
 
 # Define the Standard App Service Plan
